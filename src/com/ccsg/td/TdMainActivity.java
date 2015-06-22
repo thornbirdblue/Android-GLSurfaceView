f383e3bc77f3cbbe758c1f5a913c20b25db0b434
thornbirdblue <thornbird.blue@gmail.com>
2015-06-22 18:29:06 +0800

    origin code

---
+++ src/com/ccsg/td/TdMainActivity.java
@@ -0,0 +1,15 @@
+package com.ccsg.td;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class TdMainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_td_main);
+	}
+}
